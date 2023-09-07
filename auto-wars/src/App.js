bf56bf973e14b28f107ccced7448c73eb36f22e5
pplemover <paulimbest@naver.com>
2023-09-07 18:16:13 +0900

    react-styled

--- auto-wars/src/App.js
+++ auto-wars/src/App.js
@@ -1,12 +1,29 @@
 import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render(){
     return (
       <main className="main-container">
-        
+        <h1>예산 계산기</h1>
+
+        <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
+          {/* Expense Form */}
+        </div>
+
+        <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
+          {/* Expense List */}
+        </div>
+
+        <div style={{display: 'flex', justifyContent: 'end', marginTop: '1rem' }}>
+          <p style={{fontsize: '2rem'}}>
+            총지출: <span>원</span>
+          </p>
+        </div>
 
       </main>
     )
   }
 }
+
+export default App;
